98fafcd5eff73142be42c7e8c3645baa50a10292
Artem-28 <artem.mikheev.git@gmail.com>
2023-02-01 11:10:46 +0500

    create component select

--- src/components/forms/FormLogin/FormLogin.vue
+++ src/components/forms/FormLogin/FormLogin.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 import { ref } from "vue";
+import UiSelect from "@/components/ui/UiSelect/UiSelect.vue";
+import useToggle from "@/composition/toggle";
+import UiIcon from "@/components/ui/UiIcon/UiIcon.vue";
 
 /* Composition */
 // import you composition api...
@@ -17,12 +20,35 @@ import { ref } from "vue";
 
 /* Composition */
 // declare you composition api...
+const { isToggle, toggle } = useToggle();
 
 /* Data */
 // declare reactive variables...
 const email = ref(null);
 const password = ref(null);
 const error = ref(false);
+const selectValue = ref(null);
+const selectOptions = [
+  "option 1",
+  "option 2",
+  "option 3",
+  "option 4",
+  "option 5",
+];
+const selectOptionsObject = [
+  { label: "option 1", value: "opt 1" },
+  { label: "option 2", value: "opt 2" },
+  { label: "option 3", value: "opt 3" },
+  { label: "option 4", value: "opt 5" },
+  { label: "option 5", value: "opt 6" },
+];
+const selectOptionsCustomLabel = [
+  { name: "option 1", value: 1 },
+  { name: "option 2", value: 2 },
+  { name: "option 3", value: 3 },
+  { name: "option 4", value: 4 },
+  { name: "option 5", value: 5 },
+];
 
 /* Life hooks */
 // life cycle hooks...
@@ -36,6 +62,7 @@ const error = ref(false);
 
 <template>
   <form @submit.prevent class="form-login">
+    <ui-icon name="twitter" />
     <ui-input
       v-model="email"
       name="email"
@@ -53,6 +80,15 @@ const error = ref(false);
       :error="error"
       outline
     />
+    <ui-select
+      v-model:value="selectValue"
+      :label="$t('base.password')"
+      key-label="name"
+      key-value="value"
+      :options="selectOptionsCustomLabel"
+      outline
+    />
+    <pre>{{ selectValue }}</pre>
   </form>
 </template>
 
